8d7e63ff4fc57a2850c24f07f4bf13fbf631956c
Kevin Yap <me@kevinyap.ca>
2017-10-08 16:53:53 -0700

    Add simple CLI tool to display games

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate joseki;
+
+use std::env;
+
+use joseki::Game;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() <= 1 {
+        eprintln!("Usage: joseki <filename>");
+    } else {
+        println!("{}", Game::from_file(&args[1]));
+    }
+}
